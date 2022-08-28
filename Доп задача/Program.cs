2afe4fe5bf24c0d0f6c54e49eaba353be50c7a72
Anna Golikova <annagolikovast@gmail.com>
2022-08-28 11:24:14 +0400

    finish

---
+++ Доп задача/Program.cs
@@ -0,0 +1,22 @@
+﻿public class AdditionalTask
+{
+    public static void Main(string[] args)
+    {
+        Div();
+    }
+
+    static void Div()
+    {
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.Write($"Число {number} кратно 7 и 23 одновременно");
+        }
+        else 
+        {
+            Console.Write($"Число {number} не кратно 7 и 23 одновременно");
+        }
+    }
+}
