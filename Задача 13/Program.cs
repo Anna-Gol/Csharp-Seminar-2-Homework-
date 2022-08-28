7ceda99b50efc81c5a118a29fe043b7e853e12e8
Anna Golikova <annagolikovast@gmail.com>
2022-08-28 10:53:37 +0400

    13 finish

---
+++ Задача 13/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Task2
+{
+    public static void Main(string[] args)
+    {
+        ThirdNumber();
+    }
+
+    static void ThirdNumber()
+    {
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        string numberText = Convert.ToString(number);
+
+        if (numberText.Length > 2)
+        {
+            Console.WriteLine($"Третья цифра в числе {number} - " + numberText[2]);
+        }
+
+        else
+        {
+            Console.Write($"В числе {number} нет третьей цифры"); 
+        }
+    }
+
+}
