0f3211426cf437965ceefdb4067572c85bfb479f
Anna Golikova <annagolikovast@gmail.com>
2022-08-28 11:06:33 +0400

    15 finish

---
+++ Задача 15/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Task3
+{
+    public static void Main(string[] args)
+    {
+        DayOff();
+    }
+
+    static void DayOff()
+    {
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 1 && number <= 7)
+        {
+            if (number == 6 || number == 7)
+            {
+                Console.Write("It's a day off!!!");
+            }
+
+            else
+            {
+                Console.Write("It's a weekday :(");
+            }
+        }
+
+        else 
+        {
+            Console.Write("It's not the day of the week");
+        }
+
+    }
+}
