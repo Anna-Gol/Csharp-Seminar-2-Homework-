e15e5c647ea74d437d4971cb51f32edde1a276e1
Anna Golikova <annagolikovast@gmail.com>
2022-08-28 10:06:35 +0400

    10 finished

---
+++ Задача 10/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Task1
+{
+    public static void Main(string[] args)
+    {
+        SecondNumber();
+    }
+
+    static void SecondNumber()
+    {
+        Console.Write("Enter number: ");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 99 && number < 1000)
+        {
+            int result = number / 10 % 10;
+            Console.Write($"Вторая цифра числа {number} - {result}"); 
+        }
+
+        else
+        {
+            Console.Write($"Число {number} не трехзначное"); 
+        }
+    }
+}
+
+
+   
+
